39c91fde94b910548b3d1e9a0f148c71d544cadb
Alexei <senserlex@gmail.com>
2018-02-28 23:59:44 +0300

    ROS implemention for tcp client.

--- src/CommandClient.cpp
+++ src/CommandClient.cpp
@@ -4,13 +4,36 @@
 
 #include <iostream>
 
+// ROS
+#include <ros/ros.h>
+#include <brics_actuator/JointPositions.h>
+
 #include "kuka_rsi3/CommandComunication.hpp"
 #include "kuka_rsi3/TrajectoryGenerator.hpp"
 
 #define TIME_STEP 0.004
+JointVal currJntAng, desiredJntAng, ang;
+bool receive = false;
+
+void jointPositionCallback(const brics_actuator::JointPositions & msg) {
+    receive = true;
+    for (size_t i = 0; i < msg.positions.size(); ++i) {
+        if (msg.positions[i].joint_uri == "joint_a1") desiredJntAng(0) = msg.positions[i].value;
+        if (msg.positions[i].joint_uri == "joint_a2") desiredJntAng(1) = msg.positions[i].value;
+        if (msg.positions[i].joint_uri == "joint_a3") desiredJntAng(2) = msg.positions[i].value;
+        if (msg.positions[i].joint_uri == "joint_a4") desiredJntAng(3) = msg.positions[i].value;
+        if (msg.positions[i].joint_uri == "joint_a5") desiredJntAng(4) = msg.positions[i].value;
+        if (msg.positions[i].joint_uri == "joint_a6") desiredJntAng(5) = msg.positions[i].value;
+    }
+}
 
 int main(int argc, char ** argv)
 {
+    /** ROS initialization **/
+    ros::init(argc, argv, "rsi_client");
+    ros::NodeHandle nh;
+    ros::Subscriber positionCommandSubscriber = nh.subscribe("/kuka_arm/arm_controller/position_command", 1000, jointPositionCallback);
+
     /** Setup connection **/
     io_service service;
     CommandClient client("127.0.0.1", 2002, service);
@@ -25,18 +48,6 @@ int main(int argc, char ** argv)
     double v[DOF] = {10, 10, 10, 10, 10, 10};
     JointVal vel(v);
 
-    JointVal currJntAng, desiredJntAng, ang;
-
-    std::cout << "Insert value for each axis (A1, A2, ... , A6) in degree and press ENTER." << std::endl;
-    std::cout << "For example: " << std::endl;
-    std::cout << "A1: 60.1"     << std::endl;
-    std::cout << "A2: 0.1"      << std::endl;
-    std::cout << "A3: 0"        << std::endl;
-    std::cout << "A4: 90"       << std::endl;
-    std::cout << "A5: -30"      << std::endl;
-    std::cout << "A6: 60"       << std::endl;
-    std::cout << "--------------------------------------------------\n" << std::endl;
-
     std::stringstream ss("");
     std::string cmd;
 
@@ -47,30 +58,36 @@ int main(int argc, char ** argv)
 
     // time step must be the same
     TrajectoryGenerator trajGen(accel, vel, TIME_STEP);
+    ros::Rate r(100);
 
-    while (true) {
-        for (size_t i = 0; i < 6; ++i) {
-            std::cout << "A" << (i + 1) << ": "; std::cin >> desiredJntAng(i);
+    while (ros::ok()) {
+        // for (size_t i = 0; i < 6; ++i) {
+        //     std::cout << "A" << (i + 1) << ": "; std::cin >> desiredJntAng(i);
+        // }
+        // std::cout << std::endl;
+        ros::spinOnce();
+
+        if (receive) {
+            if (!trajGen.generateTrajectory(currJntAng, desiredJntAng)) return 1;
+
+            for (size_t i = 0; i < trajGen.time.size(); ++i) {
+
+                ang = trajGen.qTraj[i];
+                ss << "<AKorr A1=\"" << ang(0) << "\" A2=\"" << ang(1) << "\" A3=\"" << ang(2) << "\" A4=\"" << ang(3) << "\" A5=\"" << ang(4) << "\" A6=\"" << ang(5) << "\" />" << std::endl;
+                cmd = ss.str();
+                client.send(cmd);
+                cmd = client.read();
+                std::cout << cmd.data() << std::endl;
+                std::cout << i << "   -----------------------------------------------\n" << std::endl;
+
+                // clear
+                ss.str("");
+                boost::this_thread::sleep_for(boost::chrono::milliseconds(dt));
+            }
+            currJntAng = desiredJntAng;
+            receive = false;
         }
-        std::cout << std::endl;
-
-        if (!trajGen.generateTrajectory(currJntAng, desiredJntAng)) return 1;
-
-        for (size_t i = 0; i < trajGen.time.size(); ++i) {
-
-            ang = trajGen.qTraj[i];
-            ss << "<AKorr A1=\"" << ang(0) << "\" A2=\"" << ang(1) << "\" A3=\"" << ang(2) << "\" A4=\"" << ang(3) << "\" A5=\"" << ang(4) << "\" A6=\"" << ang(5) << "\" />" << std::endl;
-            cmd = ss.str();
-            client.send(cmd);
-            cmd = client.read();
-            std::cout << cmd.data() << std::endl;
-            std::cout << i << "   -----------------------------------------------\n" << std::endl;
-
-            // clear
-            ss.str("");
-            boost::this_thread::sleep_for(boost::chrono::milliseconds(dt));
-        }
-        currJntAng = desiredJntAng;
+        r.sleep();
     }
 
     return 0;
